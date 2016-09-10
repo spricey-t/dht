8f16bb415490e5a57b606fecdc74dc159a3f3d33
Shane Price <shane.tas.price@gmail.com>
2016-09-09 21:26:48 -0600

    started finger table -  didnt get far

--- dht-core/src/main/java/com/virohtus/dht/node/PeerManager.java
+++ dht-core/src/main/java/com/virohtus/dht/node/PeerManager.java
@@ -2,24 +2,34 @@ package com.virohtus.dht.node;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 public class PeerManager {
 
     private final ExecutorService executorService;
     private final PeerManagerDelegate peerManagerDelegate;
+    private final Map<String, Peer> peers;
 
     public PeerManager(ExecutorService executorService, PeerManagerDelegate peerManagerDelegate) {
         this.executorService = executorService;
         this.peerManagerDelegate = peerManagerDelegate;
+        this.peers = new HashMap<>();
     }
 
     public Peer createPeer(Socket socket) throws IOException {
         Peer peer = new Peer(this, executorService, socket);
+        synchronized (peers) {
+            peers.put(peer.getId(), peer);
+        }
         return peer;
     }
 
     public void onPeerDisconnect(Peer peer) {
+        synchronized (peers) {
+            peers.remove(peer.getId());
+        }
         peerManagerDelegate.peerDisconnected(peer);
     }
 }
