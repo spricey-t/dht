fa3f767a67a530de49eb251e58cd63834f0fa42a
Shane Price <shane.tas.price@gmail.com>
2016-09-15 20:29:31 -0600

    forgot to add serializers like a derp

--- dht-core/src/main/java/com/virohtus/dht/core/network/event/NodeIdentityResponse.java
+++ dht-core/src/main/java/com/virohtus/dht/core/network/event/NodeIdentityResponse.java
@@ -3,6 +3,8 @@ package com.virohtus.dht.core.network.event;
 import com.virohtus.dht.core.DhtProtocol;
 import com.virohtus.dht.core.event.Event;
 import com.virohtus.dht.core.network.NodeIdentity;
+import com.virohtus.dht.core.util.DhtInputStream;
+import com.virohtus.dht.core.util.DhtOutputStream;
 
 import java.io.IOException;
 
@@ -25,5 +27,17 @@ public class NodeIdentityResponse extends Event {
 
     public NodeIdentity getNodeIdentity() {
         return nodeIdentity;
+    }
+
+    @Override
+    public void serialize(DhtOutputStream dhtOutputStream) throws IOException {
+        super.serialize(dhtOutputStream);
+        dhtOutputStream.writeSizedData(nodeIdentity.getBytes());
+    }
+
+    @Override
+    public void deserialize(DhtInputStream dhtInputStream) throws IOException {
+        super.deserialize(dhtInputStream);
+        nodeIdentity = new NodeIdentity(dhtInputStream.readSizedData());
     }
 }
