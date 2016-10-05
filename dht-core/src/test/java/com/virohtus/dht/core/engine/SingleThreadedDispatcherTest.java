49a941201c72fb3abad9d9baed90b90c68a2235f
Shane Price <shane.tas.price@gmail.com>
2016-10-04 22:34:05 -0600

    started engine tests

---
+++ dht-core/src/test/java/com/virohtus/dht/core/engine/SingleThreadedDispatcherTest.java
@@ -0,0 +1,30 @@
+package com.virohtus.dht.core.engine;
+
+import org.junit.Test;
+
+public class SingleThreadedDispatcherTest {
+
+    @Test
+    public void testStartStop() {
+    }
+
+    @Test
+    public void testDispatch() {
+    }
+
+    @Test
+    public void testDispatchNewStoreAdded() {
+    }
+
+    @Test
+    public void testRegisterStore() {
+    }
+
+    @Test
+    public void testUnregisterStore() {
+    }
+
+    @Test
+    public void testListStores() {
+    }
+}
