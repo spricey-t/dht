362864d5b8ae4f184b19ef63e5bc5a2cfe28b885
Shane <shane.tas.price@gmail.com>
2016-11-04 22:49:01 -0600

    removed unused code

--- dht-transport/src/main/java/com/virohtus/dht/transport/Wireable.java
+++
@@ -1,9 +0,0 @@
-package com.virohtus.dht.transport;
-
-import com.virohtus.dht.transport.serialize.TransportInputStream;
-import com.virohtus.dht.transport.serialize.TransportOutputStream;
-
-public interface Wireable {
-    void toWire(final TransportOutputStream outputStream);
-    void fromWire(final TransportInputStream inputStream);
-}
